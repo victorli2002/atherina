fe8426c75f7a3da948ab5019c629afe098c32c3a
victorli2002 <vli2002@rocketmail.com>
2026-03-08 18:46:49 -0700

    huh

--- examples/test.py
+++ examples/test.py
@@ -27,15 +27,15 @@ def bazz(a: List[int] | str):
     raise Exception("not supposed to get triggered")
 
 @fuzz
-def qux(a: Dict[int, complex], b: Set[int], c: Tuple[int, float, complex]):
-    assert len(a) > 3
-    assert len(b) > 3
-    if any(v.imag > 1000 for v in a.values()):
-        raise Exception
-    if any(x < 0 for x in b) and sum(b) > 100:
-        raise Exception
-    if c[1] > 50 and abs(c[2]) < 1:
-        raise Exception
+def qux(a: Dict[int, complex], b: Set[int], c: Tuple[int, float]):
+    # if any(v.imag > 1000 for v in a.values()):
+    #     raise Exception
+    #     return
+    # if sum(b) > 100:
+    #     raise Exception
+    if c[0] % 100 == 7:
+        if c[1] > c[0]:
+            raise Exception
 
 if __name__ == "__main__":
     fuzz_all()
